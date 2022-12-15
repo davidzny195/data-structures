66b86a9789ce65e0719313789558e5ce4954c6f2
David Zhang <david.zny1022@gmail.com>
2022-12-14 17:36:39 -0800

    Finish sprint 1

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.count) {
+      this.count--;
+    }
+
+    var result = this.storage[0];
+    delete this.storage[0];
+
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[this.count];
+
+    return result;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
